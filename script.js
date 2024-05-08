d8901e0024b0dc29869dc749936a3d5f1f816ea1
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-05-08 14:15:49 -0400

    Changes not staged for commit: 	modified:   index.html 	modified:   places.html 	modified:   script.js 	modified:   styles.css
    Untracked files:
    	fall1.jpg
    	fall2.jpg
    	food1.jpg
    	food2.jpg
    	gem3.jpg
    	histo1.jpg

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+var prevScrollpos = 0; // Declare and initialize the prevScrollpos variable
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
     var currentScrollPos = window.scrollY;
+    console.log("prevScrollpos:", prevScrollpos);
+    console.log("currentScrollPos:", currentScrollPos);
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
         document.getElementById("navbar").style.top = "-50px";
     }
     prevScrollpos = currentScrollPos;
